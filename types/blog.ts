3ff6fc1147a581e40c91aa3b9e2559d40732473d
John Lee <drumwolf@Johns-MacBook-Pro.local>
2026-01-23 08:34:20 -0800

    Create Type Definitions

---
+++ types/blog.ts
@@ -0,0 +1,16 @@
+export interface PostMetadata {
+  title: string
+  date: string
+  description: string
+  author?: string
+  tags?: string[]
+  category: 'posts' | 'interviews' | 'scenes'
+  band?: string
+  location?: string
+}
+
+export interface Post extends PostMetadata {
+  slug: string
+  content: string
+  readingTime: string
+}
