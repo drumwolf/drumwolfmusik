b17f2d9228e33a2e673199642f9601a3299897c9
John Lee <drumwolf@Johns-MacBook-Pro.local>
2026-01-25 17:53:21 -0800

    YouTube embed - constrain max height and width

--- components/YouTubeEmbed.tsx
+++ components/YouTubeEmbed.tsx
@@ -1,11 +1,10 @@
 export function YouTubeEmbed({ videoId }: { videoId: string }) {
   return (
-    <div className="relative aspect-video w-full my-4">
+    <div className="relative aspect-video w-full max-w-[560px] my-4">
       <iframe 
         className="absolute top-0 left-0 w-full h-full" 
         src={`https://www.youtube.com/embed/${videoId}`}
-        title="YouTube video player" 
-        // frameBorder="0" 
+        title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
         referrerPolicy="strict-origin-when-cross-origin" 
         allowFullScreen
