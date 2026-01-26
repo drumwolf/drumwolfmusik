2d88c9c7bd9ce873741bb36994e889a2bf61f21c
John Lee <macmini@Johns-Mac-mini-2.local>
2026-01-25 20:37:02 -0800

    added Google Analytics code

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import Header from '@/components/Header'
 import type { Metadata } from "next";
+import Script from "next/script"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,19 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0QXYRPWDR0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0QXYRPWDR0');
+          `}
+        </Script>
         <Header />
         <main className="max-w-4xl mx-auto font-sans bg-white min-h-screen">
           {children}
