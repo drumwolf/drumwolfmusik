bc8103c15ed208adcee53749a2b1d11fde897eee
John Lee <macmini@Johns-Mac-mini-2.local>
2026-01-26 16:55:39 -0800

    fixed header in inside pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,8 +9,8 @@ const Header = () => {
   if (pathname === '/') return null
 
   return (
-    <header className="relative border-b border-gray-200 overflow-hidden">
-      <div className="relative h-20">
+    <header className="relative overflow-hidden">
+      <div className="relative h-20 max-w-4xl mx-auto"> {/* Add max-w-4xl mx-auto here */}
         {/* Background Image */}
         <Image 
           src="/images/cover_photo.jpg" 
@@ -24,7 +24,7 @@ const Header = () => {
         <div className="absolute inset-0 bg-black/30 z-10"></div>
         
         {/* Navigation Content */}
-        <nav className="relative z-20 h-full max-w-4xl mx-auto px-8 flex items-center">
+        <nav className="relative z-20 h-full px-8 flex items-center">
           <div className="flex justify-between items-center font-sans w-full">
             <Link href="/" className="text-2xl font-display font-black drop-shadow-lg leading-[0.85] tracking-tighter text-white">drumwolfmusik</Link>
             <Link href="/posts" className="hover:text-blue-400 text-white drop-shadow-lg leading-none">Articles</Link>
